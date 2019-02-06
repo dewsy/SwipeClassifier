87d5697b05ed1c31166c5100235ac8915c8e4960
Dewsy <dewsyy@gmail.com>
2019-02-06 11:12:03 +0100

    dataset class created

---
+++ lib/dataset.dart
@@ -0,0 +1,11 @@
+import 'package:multi_image_picker/asset.dart';
+
+
+class Dataset {
+  String name;
+  String rightSwipeName;
+  String rightSwipeTag;
+  String leftSwipeName;
+  String leftSwipeTag;
+  List<Asset> images;
+}

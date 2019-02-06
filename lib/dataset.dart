5118a91e2c422dc0511924cb0b0fd42a7af39d06
Dewsy <dewsyy@gmail.com>
2019-02-06 12:44:43 +0100

    Datasets are now savable to SharedPreferences

--- lib/dataset.dart
+++ lib/dataset.dart
@@ -1,4 +1,7 @@
 import 'package:multi_image_picker/asset.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
+import 'dart:convert';
 
 
 class Dataset {
@@ -16,6 +19,38 @@ Dataset(String name, String, rightSName, String rightSTag, String leftSName, Str
   this.leftSwipeName = leftSName;
   this.leftSwipeTag = leftSTag;
   this.images = images;
+}
+
+Dataset.fromJson(String jsonString) {
+  Map<String, dynamic> json = jsonDecode(jsonString);
+  this.name = json['name'];
+  this.rightSwipeName = json['rightSwipeName'];
+  this.rightSwipeTag = json['rightSwipeTag'];
+  this.leftSwipeName = json['leftSwipeName'];
+  this.leftSwipeTag = json['leftSwipeTag'];
+  this.images = json['images'];
+}
+
+// TODO: move this to a class outside of Dataset
+Future<Dataset> loadDatasetFromStorage(String name) async {
+  final prefs = await SharedPreferences.getInstance();
+  String jsonString = prefs.getString(name);
+  return Dataset.fromJson(jsonString);
+}
+
+Future<void> saveDataset() async {
+   Map<String, dynamic> jsonMap =
+  {
+    'name' : this.name,
+    'rightSwipeName' : this.rightSwipeName,
+    'rightSwipeTag' : this.rightSwipeTag,
+    'leftSwipeName' : this.leftSwipeName,
+    'leftSwipeTag' : this.leftSwipeTag,
+    'images' : this.images,
+  };
+  String jsonString = jsonEncode(jsonMap);
+  final prefs = await SharedPreferences.getInstance();
+  prefs.setString(this.name, jsonString);
 }
 
 }

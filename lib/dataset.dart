79c92fbf4330bfd66c8b9edf978eb3686f3a7e68
Dewsy <dewsyy@gmail.com>
2019-02-06 11:21:31 +0100

    constructor added

--- lib/dataset.dart
+++ lib/dataset.dart
@@ -8,4 +8,14 @@ class Dataset {
   String leftSwipeName;
   String leftSwipeTag;
   List<Asset> images;
+
+Dataset(String name, String, rightSName, String rightSTag, String leftSName, String leftSTag, List<Asset> images) {
+  this.name =name;
+  this.rightSwipeName = rightSName;
+  this.rightSwipeTag = rightSTag;
+  this.leftSwipeName = leftSName;
+  this.leftSwipeTag = leftSTag;
+  this.images = images;
+}
+
 }

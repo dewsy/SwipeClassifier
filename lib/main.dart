7b0883be920c71e886e5630d32ab5db52bda35a2
Dewsy <dewsyy@gmail.com>
2019-02-16 07:18:31 +0100

    broke up long methods

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyApp> {
                 backgroundColor: Colors.orange[200],
                 foregroundColor: Colors.white,
                 onPressed: () {
-                  getNewDateset();
+                  _createNewDateset();
                 },
                 tooltip: 'Pick Image',
                 child: Icon(Icons.photo_library,),
@@ -56,10 +56,9 @@ class _MyHomePageState extends State<MyApp> {
   }
 
 
-  getNewDateset() async {
+  _createNewDateset() async {
     Dataset temporaryDataset = await Navigator.push(
-        context, MaterialPageRoute(builder: (context) => AddNewDataset()));
-
+    context, MaterialPageRoute(builder: (context) => AddNewDataset()));
     setState(() {
       if (temporaryDataset != null) {
         _currentDataset = temporaryDataset;
@@ -74,77 +73,31 @@ class _MyHomePageState extends State<MyApp> {
     }else if (_currentDataset.images.length  <= _currentIndex && _currentDataset.images.isNotEmpty) {
       _currentIndex = 0;
       StorageHandler().saveIndex(_currentIndex);
-      return Center(
-          child: Text(
-        "All done! Good job!",
-        style: TextStyle(fontSize: 20),
-      ));
+      return _fullscreenMessages("All done, great job!");
     } else {
-      return Center(
-          child: Text(
-        "Add dataset with the button below",
-        style: TextStyle(fontSize: 20),
-      ));}
+      return _fullscreenMessages("Add dataset with the button below");}
   }
-  
+
+  Widget _fullscreenMessages(String message) {
+    return Center(
+      child: Text(
+      "$message",
+      style: TextStyle(fontSize: 20),
+      ));
+  }
 
   Widget _stack() {
     return Stack(
       children: <Widget>[
-        Row(
-          children: <Widget>[
-            Container(
-              // color: Colors.red,
-              width: MediaQuery.of(context).size.width / 2,
-              child: DragTarget(builder: (context, candidate, rejected) {
-                return Container(
-                  margin: EdgeInsets.only(top: 10, left: 10),
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    _currentDataset.leftSwipeName,
-                    style: TextStyle(fontSize: 30, color: Colors.grey),
-                  ),
-                );
-              }, onWillAccept: (data) {
-                return true;
-              }, onAccept: (data) {
-                print('Accepted LEFT!');
-                File thisImage = _currentDataset.images[_currentIndex];
-                _fileRenamer(thisImage, _currentDataset.leftSwipeTag);
-                ++_currentIndex;
-                _reloadState();
-              }),
-            ),
-            Container(
-              // color: Colors.blue,
-              width: MediaQuery.of(context).size.width / 2,
-              child: DragTarget(builder: (context, candidate, rejected) {
-                return Container(
-                  margin: EdgeInsets.only(top: 10, right: 10),
-                  alignment: Alignment.topRight,
-                  child: Text(
-                    _currentDataset.rightSwipeName,
-                    style: TextStyle(fontSize: 30, color: Colors.grey),
-                  ),
-                );
-              }, onWillAccept: (data) {
-                return true;
-              }, onAccept: (data) {
-                print('Accepted RIGHT!');
-                File thisImage = _currentDataset.images[_currentIndex];
-                _fileRenamer(thisImage, _currentDataset.rightSwipeTag);
-                ++_currentIndex;
-                _reloadState();
-              }),),],),
-        Container( child:
-        _cardSwipes(),
+        Row( children: _dropTargets(),),
+        Container( child: _draggableCard(),
         margin: EdgeInsets.only(top: 70),
         )
       ],
     );}
 
 
-  Widget _cardSwipes() {
+  Widget _draggableCard() {
     return 
     Container(
       alignment: Alignment.center,
@@ -171,26 +124,17 @@ class _MyHomePageState extends State<MyApp> {
           ),)
         ]));
   }
-
-  
-
-  Future<void> _reloadState() async{
-    await StorageHandler().saveIndex(_currentIndex).then((i) {
-      setState(() {
-      });
-    });
-  }
-
   
 
   String _pageTitle() {
     String _name = _currentDataset.name;
-    if (_name.length > 0) {
+    if (_name != '') {
       return _name;
     } else {
       return 'Swipe Classifier';
     }
   }
+
 
   Future<Dataset> _getLatestDataset() async {
     _currentIndex = await StorageHandler().loadIndex();
@@ -198,10 +142,74 @@ class _MyHomePageState extends State<MyApp> {
     return StorageHandler().loadLatestDataset();
   }
 
+
   _fileRenamer(File file, String tag) async {
     //TODO: Implement warning for too many dots
     List<String> splitname = file.path.split(".");
     await StorageHandler().getPermission().then(
         (onValue) => file.rename('${splitname[0] + tag + "." + splitname[1]}'));
+  }
+
+
+  List<Widget> _dropTargets() {
+    return <Widget> [
+      _leftDropTarget(),
+      _rightDropTarget()
+    ];
+  }
+
+
+  Widget _leftDropTarget() {
+    return Container(
+      width: MediaQuery.of(context).size.width / 2,
+      child: DragTarget(builder: (context, candidate, rejected) {
+        return Container(
+          margin: EdgeInsets.only(top: 10, left: 10),
+          alignment: Alignment.topLeft,
+          child: Text(
+            _currentDataset.leftSwipeName,
+            style: TextStyle(fontSize: 30, color: Colors.grey),
+          ),
+        );
+      }, onWillAccept: (data) {
+        return true;
+      }, onAccept: (data) {
+        print('Accepted LEFT!');
+        File _thisImage = _currentDataset.images[_currentIndex];
+        _fileRenamer(_thisImage, _currentDataset.leftSwipeTag);
+        ++_currentIndex;
+        _reloadState();
+      }),
+    );
+  }
+
+
+  Widget _rightDropTarget() {
+    return Container(
+      width: MediaQuery.of(context).size.width / 2,
+      child: DragTarget(builder: (context, candidate, rejected) {
+        return Container(
+          margin: EdgeInsets.only(top: 10, right: 10),
+          alignment: Alignment.topRight,
+          child: Text(
+            _currentDataset.rightSwipeName,
+            style: TextStyle(fontSize: 30, color: Colors.grey),
+          ),
+        );
+      }, onWillAccept: (data) {
+        return true;
+      }, onAccept: (data) {
+        print('Accepted RIGHT!');
+        File _thisImage = _currentDataset.images[_currentIndex];
+        _fileRenamer(_thisImage, _currentDataset.rightSwipeTag);
+        ++_currentIndex;
+        _reloadState();
+      }),);
+  }
+
+  Future<void> _reloadState() async{
+    await StorageHandler().saveIndex(_currentIndex).then((i) {
+    setState(() {});
+    });
   }
 }
